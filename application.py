41f2404522e5086f1da8b025e10992e192285dba
unknown <vallee.neutral@gmail.com>
2024-11-20 23:37:35 +0000

    Prediction Pipeline

---
+++ application.py
@@ -0,0 +1,75 @@
+from flask import Flask,request,render_template, jsonify
+from flask_cors import CORS,cross_origin
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+app=application
+
+@app.route('/')
+@cross_origin()
+def index(): 
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+@cross_origin()
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('index.html')
+    else:
+        data=CustomData(
+            longitude=float(request.form.get('longitude')),
+            latitude=float(request.form.get('latitude')),
+            housing_median_age=request.form.get('housing_median_age'),
+            total_rooms=request.form.get('total_rooms'),
+            total_bedrooms=request.form.get('total_bedrooms'),
+            population=request.form.get('population'),
+            households=request.form.get('households'),
+            median_income=float(request.form.get('median_income')),
+            ocean_proximity=request.form.get('ocean_proximity'),
+        ) 
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)        
+
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
+        return render_template('index.html',results=results[0])
+
+@app.route('/predictAPI',methods=['POST'])
+@cross_origin()
+def predict_api():
+    if request.method=='GET':
+        return render_template('index.html')
+    else:
+        data=CustomData(
+            longitude=float(request.json('longitude')),
+            latitude=float(request.json('latitude')),
+            housing_median_age=request.json('housing_median_age'),
+            total_rooms=request.json('total_rooms'),
+            total_bedrooms=request.json('total_bedrooms'),
+            population=request.json('population'),
+            households=request.json('households'),
+            median_income=float(request.json('median_income')),
+            ocean_proximity=request.json('ocean_proximity'),
+        ) 
+
+        pred_df=data.get_data_as_data_frame()      
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
+
+        dct = {'median_house_value':round(results[0],2)}
+        return jsonify(dct)   
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", debug=True) 
+
+
+
+
+
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,64 +1,75 @@
-# import sys
-# import pandas as pd
-# import os
+import sys
+import pandas as pd
+import os
 
-# # Add the project root directory to sys.path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# from src.exception import CustomException
-# from src.utils import load_object
+# Add the project root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.exception import CustomException
+from src.utils import load_object
 
-# class PredictPipeline:
-#     def __init__(self):
-#         pass
+class PredictPipeline: 
+    def __init__(self):
+        pass
 
-#     def predict(self,features):
-#         try:
-#             model_path=os.path.join("artifacts","model.pkl")
-#             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-#             print("Before Loading")
-#             model=load_object(file_path=model_path)
-#             preprocessor=load_object(file_path=preprocessor_path)
-#             # print("After Loading")
-#             print("Features before preprocessing:")
-#             print(features)            
-#             data_scaled=preprocessor.transform(features)
-#             preds=model.predict(data_scaled)
-#             return preds
+    # model prediction pipe
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            print("Features before preprocessing:")
+            print(features)            
+            data_scaled=preprocessor.transform(features)
+            print("Features after scaling:")
+            print(data_scaled)
+            preds=model.predict(data_scaled)
+            return preds
         
-#         except Exception as e:
-#             raise CustomException(e,sys)
+        except Exception as e:
+            raise CustomException(e,sys)
 
-# class CustomData:
-#     def __init__(self,
-#         gender: str,
-#         race_ethnicity: str,
-#         parental_level_of_education,
-#         lunch: str,
-#         test_preparation_course: str,
-#         reading_score: int,
-#         writing_score: int):
+class CustomData:
+    def __init__(self,
+        longitude: float,
+        latitude: float,
+        housing_median_age:int,
+        total_rooms: int,
+        total_bedrooms: int,
+        population: int,
+        households: int,
+        median_income:float,
+        ocean_proximity
+        ):
 
-#         self.gender = gender
-#         self.race_ethnicity = race_ethnicity
-#         self.parental_level_of_education = parental_level_of_education
-#         self.lunch = lunch
-#         self.test_preparation_course = test_preparation_course
-#         self.reading_score = reading_score
-#         self.writing_score = writing_score       
+        self.longitude = longitude
+        self.latitude = latitude
+        self.housing_median_age = housing_median_age
+        self.total_rooms = total_rooms
+        self.total_bedrooms = total_bedrooms
+        self.population = population
+        self.households = households       
+        self.median_income = median_income       
+        self.ocean_proximity = ocean_proximity       
 
-#     def get_data_as_data_frame(self):
-#         try:
-#             custom_data_input_dict = {
-#                 "gender": [self.gender],
-#                 "race_ethnicity": [self.race_ethnicity ],
-#                 "parental_level_of_education": [self.parental_level_of_education],
-#                 "lunch": [self.lunch],
-#                 "test_preparation_course": [self.test_preparation_course ],
-#                 "reading_score": [self.reading_score],
-#                 "writing_score": [self.writing_score],
-#             }
+    # return all input in form of a dataframe
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "longitude": [self.longitude],
+                "latitude": [self.latitude ],
+                "housing_median_age": [self.housing_median_age],
+                "total_rooms": [self.total_rooms],
+                "total_bedrooms": [self.total_bedrooms ],
+                "population": [self.population],
+                "households": [self.households],
+                "median_income": [self.median_income],
+                "ocean_proximity": [self.ocean_proximity],
+            }
 
-#             return pd.DataFrame(custom_data_input_dict)
+            return pd.DataFrame(custom_data_input_dict)
 
-#         except Exception as e:
-#             raise CustomException(e, sys) 
+        except Exception as e:
+            raise CustomException(e, sys) 
